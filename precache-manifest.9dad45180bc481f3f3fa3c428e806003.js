fd195e5274635d5f304547041b810b7ecb9ce854
Khaeljy <contact@khaeljy.fr>
2020-12-28 16:08:00 +0100

    Updates

---
+++ precache-manifest.9dad45180bc481f3f3fa3c428e806003.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e6d14c35f049f6b3b9b7301a51060d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "322e0e575dfbfcf06b7b",
+    "url": "/static/css/2.892654cc.chunk.css"
+  },
+  {
+    "revision": "01cae76e805b4f90d47e",
+    "url": "/static/css/main.c50feda6.chunk.css"
+  },
+  {
+    "revision": "322e0e575dfbfcf06b7b",
+    "url": "/static/js/2.dc9d0ae4.chunk.js"
+  },
+  {
+    "revision": "ed364e94a6f6b1bb953368935d752ffd",
+    "url": "/static/js/2.dc9d0ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cae76e805b4f90d47e",
+    "url": "/static/js/main.80cd8b71.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54d320bc8337a0ee36d9e0aac5ac56c3.js"
+  "/precache-manifest.9dad45180bc481f3f3fa3c428e806003.js"
 );
 
 self.addEventListener('message', (event) => {
